180904d42fd3d4b663062639dc62b37a89047673
Tianrui Li <lsfans@qq.com>
2024-06-27 12:57:51 +0800

    - ChainsawDaq 0.0.1: XDMA + UserRegisterSpace

---
+++ chainsaw/src/main/scala/chainsaw/projects/xdma/daq/ChainsawDaq.scala
@@ -0,0 +1,67 @@
+package chainsaw.projects.xdma.daq
+
+import spinal.core._
+import spinal.lib._
+import spinal.lib.blackbox.xilinx.ultrascale.{IBUFDS, OBUFDS}
+import spinal.lib.bus.misc.SizeMapping
+import spinal.lib.bus.regif.AccessType._
+import spinal.lib.bus.regif._
+import spinal.lib.eda.xilinx.boards.alinx.Axku062
+import spinal.lib.blackbox.xilinx.s7.IBUF
+import spinal.lib.bus.amba4.axilite.AxiLite4
+
+import scala.language.postfixOps
+
+// FIXME: when output set by asOutput is not driven, elaboration won't failed, this may lead to bad output pins
+// FIXME: base addr has no effect on the generated RTL, when base addr is not 0, you should set register address starting from base address
+
+case class ChainsawDaq() extends Axku062 {
+
+  val peripheral = new Peripheral_wrapper()
+  peripheral.sys_clk_200M := defaultClockDomain.clock
+
+  peripheral.pcie_clk_clk_n := pcie.clk_n
+  peripheral.pcie_clk_clk_p := pcie.clk_p
+
+  val ibuf = IBUF()
+  ibuf.I := pcie.perst
+  peripheral.pcie_rstn := ibuf.O
+
+  peripheral.pcie_mgt_rxn := pcie.rx_n
+  peripheral.pcie_mgt_rxp := pcie.rx_p
+  pcie.tx_n := peripheral.pcie_mgt_txn
+  pcie.tx_p := peripheral.pcie_mgt_txp
+
+  val controlClockingArea = new ClockingArea(defaultClockDomain) {
+    val registerSpace = ChainsawDaqRegisterSpace(peripheral.m_axi_lite_user)
+
+    // LEDs for system status
+    val pcieHeartBeat = CounterFreeRun(1 << 25)
+
+    led.clearAll()
+    led(0) := pcieHeartBeat.msb
+  }
+
+}
+
+case class ChainsawDaqRegisterSpace(user: AxiLite4) extends Area {
+
+  val user_base_addr = 0x00000
+  val userBusIf = AxiLite4BusInterface(user, SizeMapping(user_base_addr, 0x10000))
+
+  val versionReg = userBusIf.newReg("firmware version")
+  val revision = versionReg.field(UInt(8 bits), RO)
+  val minor = versionReg.field(UInt(8 bits), RO)
+  val major = versionReg.field(UInt(8 bits), RO)
+  revision := 0x00
+  minor := 0x01
+  major := 0x00
+
+  val rwTestReg = userBusIf.newReg("reserved RW field for testing AXI4-Lite read/write")
+  val rwTest = rwTestReg.field(Bits(32 bits), RW)
+
+  userBusIf.accept(HtmlGenerator("UserRegisterSpace", "AP"))
+  userBusIf.accept(CHeaderGenerator("UserRegisterSpace", "AP"))
+  userBusIf.accept(PythonHeaderGenerator("UserRegisterSpace", "AP"))
+
+}
---
+++ chainsaw/src/main/scala/chainsaw/projects/xdma/daq/ToSpinalHDL.scala
@@ -0,0 +1,119 @@
+package chainsaw.projects.xdma.daq
+
+import spinal.core._
+import spinal.core.{GlobalData, SpinalError}
+import spinal.lib.bus.regif._
+
+import java.io.PrintWriter
+import scala.collection.mutable
+
+case class Ddr4Interface() extends Bundle {
+
+  // TODO: parameterized bitwidth
+  val act_n = out Bool ()
+  val adr = out Bits (17 bits)
+  val ba = out Bits (2 bits)
+  val bg = out Bool ()
+  val ck_c = out Bool ()
+  val ck_t = out Bool ()
+  val cke = out Bool ()
+  val cs_n = out Bool ()
+  val dm_n = inout(Analog(Bits(8 bits)))
+  val dq = inout(Analog(Bits(64 bits)))
+  val dqs_c = inout(Analog(Bits(8 bits)))
+  val dqs_t = inout(Analog(Bits(8 bits)))
+  val odt = out Bool ()
+  val reset_n = out Bool ()
+
+}
+
+case class PythonHeaderGenerator(
+    fileName: String,
+    prefix: String,
+    regType: String = "u32"
+) extends BusIfVisitor {
+  val words = "\\w*".r
+  prefix match {
+    case words(_*) => null
+    case _         => SpinalError(s"${prefix} should be Valid naming : '[A-Za-z0-9_]+'")
+  }
+
+  case class Reg(name: String, addr: Long)
+
+  case class Field(name: String, width: Long, accessType: AccessType)
+
+  case class Type(name: String, var fields: List[FieldDescr])
+
+  val regs: mutable.ListBuffer[RegDescr] = mutable.ListBuffer[RegDescr]()
+  val types: mutable.ListBuffer[Type] = mutable.ListBuffer[Type]()
+  var regLength: Int = 0
+
+  def begin(busDataWidth: Int): Unit = {}
+
+  def visit(descr: BaseDescriptor): Unit = {
+    descr match {
+      case descr: RegDescr => regDescrVisit(descr)
+      case _               => ???
+    }
+  }
+
+  private def regDescrVisit(descr: RegDescr): Unit = {
+    def nameLen = descr.getName.length()
+
+    if (nameLen > regLength)
+      regLength = nameLen
+
+    regs += descr
+    types += Type(descr.getName, descr.getFieldDescrs)
+  }
+
+  def end(): Unit = {
+    val pc = GlobalData.get.phaseContext
+    val targetPath = s"${pc.config.targetDirectory}/${fileName}.py"
+    val pw = new PrintWriter(targetPath)
+
+    pw.write(s"""
+         |class Register32:
+         |    address: int
+         |    field_widths: list[int]
+         |
+         |    def to_value(self):
+         |        value = 0
+         |        shift = 0
+         |        for (field_name, width) in zip(self.__dict__.keys(), self.field_widths):
+         |            value += (getattr(self, field_name) << shift)
+         |            shift += width
+         |        return value
+         |
+         |    def from_value(self, value):
+         |        for field_name, width in zip(self.__dict__.keys(), self.field_widths):
+         |            field_value = value % (1 << width)
+         |            setattr(self, field_name, field_value)
+         |            value >>= width
+         |
+         |""".stripMargin)
+
+    regs.zip(types).foreach { case (reg, t) =>
+      val regAddress = s"0x%0${4}x".format(reg.getAddr)
+      val fieldWidths = t.fields.map(_.getWidth)
+      val fieldOffsets = fieldWidths.scan(0)(_ + _).init
+      val fieldNames = t.fields.filterNot(_.getAccessType() == AccessType.NA).map(_.getName)
+
+      // a class supporting read/write for each reg
+      // TODO: read methods for different fields of a register
+      pw.write(s"""
+                  |class ${t.name.toLowerCase()}(Register32):
+                  |    address = ${regAddress}
+                  |    field_widths = [${fieldWidths.mkString(", ")}]
+                  |
+                  |    def __init__(self, ${fieldNames.map(name => f"${name}=0").mkString(", ")}):
+                  |        ${fieldNames.map(name => s"self.$name = $name").mkString("\n        ")}
+                  |
+                  |""".stripMargin)
+    }
+
+    val regByteCount: Int = types.head.fields.map(_.getWidth()).sum / 8
+
+    pw.close()
+  }
+}
